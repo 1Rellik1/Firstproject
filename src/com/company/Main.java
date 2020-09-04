bc31dc8cb482a71820b5b1a888f1ed4c6656826f
Alex <Alexzz123@yandex.ru>
2020-09-04 10:27:59 +0300

    Start of the profect
    Created main class

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=5;
+        boolean b = true;
+        Scanner scan= new Scanner(System.in);
+        int c;
+        c= scan.nextInt();
+        System.out.println(c+a);
+    }
+}
