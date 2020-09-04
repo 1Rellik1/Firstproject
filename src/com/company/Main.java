edb05d15f7ce036ea00e126e88d1f7bd1dd77fc3
Alex <Alexzz123@yandex.ru>
2020-09-04 11:04:23 +0300

    Task3
    Did task number 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int a=5;
+        ////////////Task3/////////////
+        int sum_for=0,sum_while=0,sum_do_while=0;
         boolean b = true;
         Scanner scan= new Scanner(System.in);
-        int c;
-        c= scan.nextInt();
-        System.out.println(c+a);
+        int[] mas= new int[5];
+        for (int i=0;i<5;i++){
+            mas[i]= scan.nextInt();
+        }
+        for (int i=0;i < 5;i++){
+            sum_for=sum_for+mas[i];
+        }
+        int count_while=0, count_do_while=0;
+        while (count_while<5){
+            sum_while=mas[count_while]+sum_while;
+            count_while++;
+        }
+        do{
+            sum_do_while=mas[count_do_while]+sum_do_while;
+            count_do_while++;
+        }while(count_do_while!=5);
+        System.out.println(sum_do_while+"  "+sum_for+"  "+ sum_while);
+
+        ///////////////Task4////////////////
     }
 }
